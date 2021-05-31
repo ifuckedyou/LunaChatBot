a402052404c72f87f33b08f561b7de9de7bff509
canbeanywhere <76033254+canbeanywhere@users.noreply.github.com>
2021-05-31 14:49:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 owner_id = 783435461
-bot_token = "1525865826:AAEF6gi4eC__jSoxIeYZ665KErJ9u3jZCXE"
-bot_id = 1525865826
+bot_token = "1837098219:AAEhxpWGpggTwCAlwpffdeUvMCj_ivc9Yec"
+bot_id = 1837098219
 ARQ_API_KEY = "JMXVTV-FBDCJX-ZZORZX-OIYNWM-ARQ"
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
