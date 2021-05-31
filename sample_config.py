6325a512922a8c7f7de7f33a4c1d7a1944ae8f42
canbeanywhere <76033254+canbeanywhere@users.noreply.github.com>
2021-05-31 14:28:13 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
-owner_id = 1243703097
-bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
-bot_id = 1663901971
+owner_id = 783435461
+bot_token = "1525865826:AAEF6gi4eC__jSoxIeYZ665KErJ9u3jZCXE"
+bot_id = 1525865826
 ARQ_API_KEY = "Get this from @ARQBot"
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
